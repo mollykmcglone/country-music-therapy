2317fabf5293bf93a8624ef26f466305ed684281
Molly <mollykmcglone@gmail.com>
2016-08-26 08:33:35 -0700

    moved destroy question action into update-question component

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -14,6 +14,11 @@ export default Ember.Component.extend({
       };
       this.set('updateQuestionForm', false);
       this.sendAction('update', question, params);
+    },
+    delete(question) {
+      if (confirm('Are you sure you want to delete this question?')){
+        this.sendAction('destroyQuestion', question);
+      }
     }
   }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -5,11 +5,6 @@ export default Ember.Route.extend({
     return this.store.findRecord('question', params.question_id);
   },
   actions: {
-    delete(question) {
-      if (confirm('Are you sure you want to delete this question?')){
-        this.sendAction('destroyQuestion', question);
-      }
-    },
     save3(params) {
       var newResponse = this.store.createRecord('response', params);
       var question = params.question;
